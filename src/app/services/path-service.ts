f012e97cdae0ab9ec4ae111c85aa0d0181ee9ad4
Achraf Sikal <achrafsikal@gmail.com>
2026-02-18 15:38:31 +0000

    feat: Enhance path service to prevent rapid point additions based on time and distance thresholds.

--- src/app/services/path-service.ts
+++ src/app/services/path-service.ts
@@ -6,7 +6,7 @@ import * as L from 'leaflet';
 })
 export class PathService {
   private readonly STORAGE_KEY = 'user_path_trace';
-
+  private lastSaveTime = 0;
   getSavedPath(): L.LatLng[] {
     const data = localStorage.getItem(this.STORAGE_KEY);
     if (!data) return [];
@@ -15,12 +15,20 @@ export class PathService {
   }
 
   addPoint(latlng: L.LatLng): void {
+    const now = Date.now();
     const currentPath = this.getSavedPath();
+    const lastPoint = currentPath[currentPath.length - 1];
+    const smallPeriod = now - this.lastSaveTime < 1000;
+    const smallDistance = lastPoint && lastPoint.distanceTo(latlng) < 1;
+    if (smallPeriod || smallDistance) {
+        return;
+    }
     
     currentPath.push(latlng); 
     
     const simplePath = currentPath.map(p => ({ lat: p.lat, lng: p.lng }));
     localStorage.setItem(this.STORAGE_KEY, JSON.stringify(simplePath));
+    this.lastSaveTime = now;
   }
 
   clearPath() {
