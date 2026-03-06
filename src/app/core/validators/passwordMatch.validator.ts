cec666afc0db7311aab9e7719065679edf0a0410
Achraf Sikal <achrafsikal@gmail.com>
2026-03-06 10:34:21 +0000

    fix: Improve password matching validation logic to set and clear errors appropriately in the form control for better user feedback.

--- src/app/core/validators/passwordMatch.validator.ts
+++ src/app/core/validators/passwordMatch.validator.ts
@@ -1,10 +1,20 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export const passwordMatchValidator: ValidatorFn =
-    (control: AbstractControl,): ValidationErrors | null =>
-{
+    (control: AbstractControl): ValidationErrors | null => {
     const password = control.get('password')?.value;
-    const confirmPassword = control.get('confirmPassword')?.value;
+    const confirmPasswordControl = control.get('confirmPassword');
+    const confirmPassword = confirmPasswordControl?.value;
 
-    return password === confirmPassword ? null : { passwordMismatch: true };
+    if (password !== confirmPassword) {
+        confirmPasswordControl?.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+    } else {
+        const errors = confirmPasswordControl?.errors;
+        if (errors) {
+            delete errors['passwordMismatch'];
+            confirmPasswordControl?.setErrors(Object.keys(errors).length ? errors : null);
+        }
+        return null;
+    }
 };
