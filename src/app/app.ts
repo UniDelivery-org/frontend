de30ed3451c093983dbe749aab79ec022b3cca4a
Achraf Sikal <achrafsikal@gmail.com>
2026-03-09 03:22:10 +0000

    feat: Enhance app initialization by checking authentication status and loading user profile if authenticated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,28 @@
-import { Component, signal, AfterViewInit } from '@angular/core';
+import { Component, signal, AfterViewInit, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Lenis from 'lenis';
+import { AuthHelper } from './core/helpers/auth.helper';
+import { Store } from '@ngrx/store';
+import { profileActions } from './features/profile/store/profile.actions';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements AfterViewInit {
+export class App implements AfterViewInit, OnInit {
   protected readonly title = signal('UniDelivery');
+  private store = inject(Store);
+  private authHelper = inject(AuthHelper);
 
+  ngOnInit(): void {
+    const isAuth = this.authHelper.isAuthenticated();
+    if (isAuth) {
+      this.store.dispatch(profileActions.loadProfile());
+    }
+  }
   ngAfterViewInit() {
     /*
     const lenis = new Lenis({
