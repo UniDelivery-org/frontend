583e6a31d7423c1bdeb02fd22e16466239cd8866
Achraf Sikal <achrafsikal@gmail.com>
2026-03-09 03:20:09 +0000

    feat: Add UpdateProfileRequestDTO interface for updating user profile information

---
+++ src/app/features/profile/data-access/update-profile.dto.ts
@@ -0,0 +1,8 @@
+export interface UpdateProfileRequestDTO {
+    fullName?: string;
+    email?: string;
+    phone?: string;
+    avatarUrl?: String;
+    currentLat?: number;
+    currentLon?: number;
+}
