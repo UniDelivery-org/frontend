3a8c0fc49846a1ef40e20e5eaa329f393e85b3ef
Achraf Sikal <achrafsikal@gmail.com>
2026-03-09 03:17:42 +0000

    refactor: Update token interceptor to conditionally attach access token based on authentication status for improved security

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,14 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CookieService } from '../services/cookie.service';
+import { AuthHelper } from '../helpers/auth.helper';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const cookie = inject(CookieService);
+  const authHelper = inject(AuthHelper);
   const accessToken = cookie.get('accessToken');
-  const clonedReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  });
-  return next(clonedReq);
+  if (authHelper.isAuthenticated()) {
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return next(clonedReq);
+  }
+  return next(req);
 };
