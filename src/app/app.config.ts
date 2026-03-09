baca57b2d501c9259f640abb4846fea4d805f17d
Achraf Sikal <achrafsikal@gmail.com>
2026-03-09 03:21:35 +0000

    feat: Integrate profile effects and token interceptor in app configuration for enhanced state management and authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,29 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
-import * as AuthEffects from './features/auth/store/auth.effect'
+import * as AuthEffects from './features/auth/store/auth.effect';
+import * as ProfileEffects from './features/profile/store/profile.effect';
 import { authFeature } from './features/auth/store/auth.reducer';
+import { profileFeature } from './features/profile/store/profile.reducer';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+    ),
     provideStore(),
     provideState(authFeature),
+    provideState(profileFeature),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideEffects(AuthEffects)
+    provideEffects(AuthEffects, ProfileEffects)
 ]
 };
